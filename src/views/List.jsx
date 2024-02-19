a6e6f5520dcb6dbb1a7309350212cfe81d65526d
etiry <7997357+etiry@users.noreply.github.com>
2024-02-19 13:42:38 -0500

    Add list filtering functionality

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,13 +1,49 @@
 import { ListItem } from '../components';
+import { useState, useEffect } from 'react';
 
 export function List({ data }) {
+	const [searchTerm, setSearchTerm] = useState('');
+	const [filteredData, setFilteredData] = useState(data);
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
+	const reset = (e) => {
+		e.preventDefault();
+		setFilteredData(data);
+		setSearchTerm('');
+	};
+
+	useEffect(() => {
+		setFilteredData(
+			data.filter((item) => {
+				const itemName = item.name.toLowerCase();
+				return itemName.includes(searchTerm.toLowerCase());
+			}),
+		);
+	}, [data, searchTerm]);
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="itemFilter">
+					Search for an item:
+					<input
+						type="text"
+						id="itemFilter"
+						name="itemFilter"
+						value={searchTerm}
+						onChange={handleChange}
+					/>
+				</label>
+				{searchTerm && <button onClick={reset}>Reset</button>}
+			</form>
 			<ul>
-				{data.map((item, index) => {
+				{filteredData.map((item) => {
 					return <ListItem key={item.id} name={item.name} />;
 				})}
 			</ul>
