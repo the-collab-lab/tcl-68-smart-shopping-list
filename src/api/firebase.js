a41ea21c2a5da5b3ab19802981a74b079f479e9f
andiedoescode <pang.andrea@gmail.com>
2024-02-23 11:16:20 -0500

    Added return truthy statement to firebase for shareList fxn

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -145,22 +145,24 @@ export async function shareList(listPath, currentUserId, recipientEmail) {
 	// Check if current user is owner.
 	if (!listPath.includes(currentUserId)) {
 		throw new Error('The list is not yours to share.');
-		return;
 	}
+
 	// Get the document for the recipient user.
 	const usersCollectionRef = collection(db, 'users');
 	const recipientDoc = await getDoc(doc(usersCollectionRef, recipientEmail));
 	// If the recipient user doesn't exist, we can't share the list.
+
 	if (!recipientDoc.exists()) {
 		throw new Error('User does not yet exist.');
-		return;
 	}
+
 	// Add the list to the recipient user's sharedLists array.
 	const listDocumentRef = doc(db, listPath);
 	const userDocumentRef = doc(db, 'users', recipientEmail);
 	updateDoc(userDocumentRef, {
 		sharedLists: arrayUnion(listDocumentRef),
 	});
+	return true;
 }
 
 /**
