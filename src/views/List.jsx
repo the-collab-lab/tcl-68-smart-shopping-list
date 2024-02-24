04f2132baa0a4aaf14dfe497e688a1e6e986c0d0
Devina Gillis <137969744+DevinaG007@users.noreply.github.com>
2024-02-24 14:54:04 -0800

    Add list filtering functionality (#24)
    * Add list filtering functionality
    
    * Refactor filtering
    
    ---------
    
    Co-authored-by: etiry <7997357+etiry@users.noreply.github.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,13 +1,43 @@
 import { ListItem } from '../components';
+import { useState } from 'react';
 
 export function List({ data }) {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
+	const reset = (e) => {
+		e.preventDefault();
+		setSearchTerm('');
+	};
+
+	const filteredData = data.filter((item) => {
+		const itemName = item.name.toLowerCase();
+		return itemName.includes(searchTerm.toLowerCase());
+	});
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="itemFilter">
+					Search for an item:
+					<input
+						type="text"
+						id="itemFilter"
+						name="itemFilter"
+						value={searchTerm}
+						onChange={handleChange}
+					/>
+				</label>
+				{searchTerm && <button onClick={reset}>Reset</button>}
+			</form>
 			<ul>
-				{data.map((item, index) => {
+				{filteredData.map((item) => {
 					return <ListItem key={item.id} name={item.name} />;
 				})}
 			</ul>
