b0a460bbe86fb8858e7dc8c62fff6aa8126e624e
Shane Misra <117242156+sdmisra@users.noreply.github.com>
2024-03-03 07:42:34 -0700

    Et sm empty list welcome (#27)
    * Conditionally render search bar only if items in list
    
    * Render welcoming prompt if list is empty
    
    * Add navigation and prompt for no list selected
    
    * refactored conditional rendering to use ternary operator
    
    ---------
    
    Co-authored-by: etiry <7997357+etiry@users.noreply.github.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,8 +1,11 @@
 import { ListItem } from '../components';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export function List({ data }) {
+export function List({ data, listPath }) {
 	const [searchTerm, setSearchTerm] = useState('');
+
+	const navigate = useNavigate();
 
 	const handleChange = (e) => {
 		setSearchTerm(e.target.value);
@@ -13,6 +16,10 @@ export function List({ data }) {
 		setSearchTerm('');
 	};
 
+	const handleClick = (selectedPath) => {
+		navigate(selectedPath);
+	};
+
 	const filteredData = data.filter((item) => {
 		const itemName = item.name.toLowerCase();
 		return itemName.includes(searchTerm.toLowerCase());
@@ -20,22 +27,39 @@ export function List({ data }) {
 
 	return (
 		<>
-			<p>
-				Hello from the <code>/list</code> page!
-			</p>
-			<form>
-				<label htmlFor="itemFilter">
-					Search for an item:
-					<input
-						type="text"
-						id="itemFilter"
-						name="itemFilter"
-						value={searchTerm}
-						onChange={handleChange}
-					/>
-				</label>
-				{searchTerm && <button onClick={reset}>Reset</button>}
-			</form>
+			{!listPath ? (
+				<>
+					<h2>
+						You haven't selected a list yet. Click below to select a list.
+					</h2>
+					<button onClick={() => handleClick('/')}>Select a list</button>
+				</>
+			) : null}
+			{listPath && data.length === 0 ? (
+				<>
+					<h2>
+						This list is currently empty. Click below to add your first item.
+					</h2>
+					<button onClick={() => handleClick('/manage-list')}>
+						Add first item
+					</button>
+				</>
+			) : null}
+			{data.length > 0 ? (
+				<form>
+					<label htmlFor="itemFilter">
+						Search for an item:
+						<input
+							type="text"
+							id="itemFilter"
+							name="itemFilter"
+							value={searchTerm}
+							onChange={handleChange}
+						/>
+					</label>
+					{searchTerm ? <button onClick={reset}>Reset</button> : null}
+				</form>
+			) : null}
 			<ul>
 				{filteredData.map((item) => {
 					return <ListItem key={item.id} name={item.name} />;
