cd9ddf9b3c433a5750408704c7946800aa2fe989
andiedoescode <pang.andrea@gmail.com>
2024-03-02 21:37:48 -0500

    Added updateTimer function to calculate remaining time, updated useEffeect to use remaining time in setTimeout

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -33,13 +33,27 @@ export function ListItem({ listPath, item }) {
 		purchaseItem();
 	};
 
+	//Calculate time remaining if purchase was less than 24 hours ago
+	const updateTimer = () => {
+		if (item.dateLastPurchased) {
+			const timeElapsed = Date.now() - item.dateLastPurchased.seconds * 1000;
+			if (timeElapsed < ONE_DAY_IN_MILLISECONDS) {
+				return ONE_DAY_IN_MILLISECONDS - timeElapsed;
+			} else {
+				return 0;
+			}
+		}
+	};
+
 	//sets a timer to uncheck an item 24 hours after it's purchased
 	useEffect(() => {
+		let timeRemaining = updateTimer();
+
 		const timer = setTimeout(() => {
-			if (purchasedOneDayAgo) {
+			if (timeRemaining > 0) {
 				setIsChecked(false);
 			}
-		}, ONE_DAY_IN_MILLISECONDS);
+		}, timeRemaining);
 		return () => clearTimeout(timer);
 	}, [isChecked, purchasedOneDayAgo]);
 
