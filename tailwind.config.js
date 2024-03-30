ba046ae77502cf97a4968bd002f4c656b247c115
Devina Gillis <devinagillis0@gmail.com>
2024-03-29 20:24:41 -0700

    added Rubik font and color pallette to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		colors: {
+			eggshell: '#FDFDF2',
+			sage: '#BBC893',
+			brown: '#D4A373',
+			yellow: '#FEFAE0',
+			vanilla: '#FAEDCD',
+			'pale-green': '#E9EDC9',
+		},
+		extend: {
+			fontFamily: {
+				Rubik: ['Rubik', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
