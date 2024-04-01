55525e2d7ff11583c6c6461a34dc44833b8f6c4d
andiedoescode <pang.andrea@gmail.com>
2024-04-01 16:21:34 -0400

    Moved colors object in theme into extend to allow use of Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		colors: {
-			eggshell: '#FDFDF2',
-			sage: '#BBC893',
-			brown: '#D4A373',
-			yellow: '#FEFAE0',
-			vanilla: '#FAEDCD',
-			'pale-green': '#E9EDC9',
-		},
 		extend: {
 			fontFamily: {
 				Rubik: ['Rubik', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				eggshell: '#FDFDF2',
+				sage: '#BBC893',
+				brown: '#D4A373',
+				yellow: '#FEFAE0',
+				vanilla: '#FAEDCD',
+				'pale-green': '#E9EDC9',
 			},
 		},
 	},
