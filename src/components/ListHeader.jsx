acdf80ca3718c5357da892de795ef02530573b3a
andiedoescode <pang.andrea@gmail.com>
2024-04-01 16:12:23 -0400

    List header bottom margin

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -1,6 +1,6 @@
 export function ListHeader({ text }) {
 	return (
-		<h2 className="flex-1 bg-sage min-h-24 text-center content-center font-bold text-4xl">
+		<h2 className="flex-1 bg-sage min-h-24 text-center content-center font-bold text-4xl mb-12">
 			{text}
 		</h2>
 	);
