3107a9b9d72b23779940a8e09b32aabb7d6ab753
andiedoescode <pang.andrea@gmail.com>
2024-04-06 23:21:26 -0400

    Adjusted margin

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -1,6 +1,6 @@
 export function ListHeader({ text }) {
 	return (
-		<h2 className="mt-6 flex-1 bg-sage min-h-24 text-center content-center font-bold text-4xl mb-12 text-off-black">
+		<h2 className="mt-4 flex-1 bg-sage min-h-24 text-center content-center font-bold text-4xl mb-12 text-off-black">
 			{text}
 		</h2>
 	);
