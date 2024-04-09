e9c1afa884e50dd2e89be0ca7520ed5f34098365
Andrea Pang <98671035+andiedoescode@users.noreply.github.com>
2024-04-09 16:35:45 -0400

    Basic conditional styling for logged out users (#66)
    * Conditional styling to home page when logged out.
    
    * Conditional styling for List and ManageList when logged out, padding adjustment to ListHeader component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,8 @@ import { SingleList } from '../components/SingleList.jsx';
 import { useState } from 'react';
 import { createList } from '../api/firebase.js';
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../api/config.js';
+import { IoBulbOutline } from 'react-icons/io5';
 
 export function Home({ data, userId, userEmail, setListPath }) {
 	const [newListName, setNewListName] = useState('');
@@ -49,38 +51,62 @@ export function Home({ data, userId, userEmail, setListPath }) {
 	};
 
 	return (
-		<div className="Home mt-8 font-Rubik mx-8 md:mx-24">
-			<form
-				onSubmit={handleSubmit}
-				className="flex rounded-lg items-center gap-x-6"
-			>
-				<label htmlFor="listName" className="text-off-black">
-					New list:
-				</label>
-				<input
-					type="text"
-					id="listName"
-					name="listName"
-					value={newListName}
-					onChange={handleChange}
-					className="border-2 border-sage rounded-lg min-h-14 bg-white grow text-xl xs:text-2xl text-off-black"
-				/>
-				<button className="bg-pale-green rounded-lg px-3 py-1 border-2 border-sage text-off-black">
-					Create
-				</button>
-			</form>
-			<ul className="flex flex-col my-8 items-center text-2xl">
-				{data.map((item, index) => {
-					return (
-						<SingleList
-							key={index}
-							name={item.name}
-							path={item.path}
-							setListPath={setListPath}
+		<div className="Home mt-8 font-Rubik mx-8 md:mx-24 h-min-content">
+			{!auth.currentUser ? (
+				<>
+					<section className="border border-dark-green py-8 px-12 text-off-black text-center tiny:mt-6 xs:mt-24 sm:mt-32 items-center justify-center bg-sage/75 rounded-2xl">
+						<h1 className="text-4xl xs:text-5xl font-semibold">
+							Welcome to Basket Buddy!
+						</h1>
+						<h2 className="italic">We're nacho average shopping list. </h2>
+					</section>
+					<section className="border border-dark-green py-8 px-12 text-off-black tiny:mt-14 items-center justify-center bg-sage/50 rounded-2xl">
+						<p className="font-semibold">
+							<IoBulbOutline className="inline h-8 text-4xl -mt-2" /> Get
+							Started :
+						</p>
+						<p>
+							Create your account by clicking the{' '}
+							<span className="font-medium">sign in</span> button above. You'll
+							be making and sharing lists in no time!
+						</p>
+					</section>
+				</>
+			) : (
+				<>
+					<form
+						onSubmit={handleSubmit}
+						className="flex rounded-lg items-center gap-x-6"
+					>
+						<label htmlFor="listName" className="text-off-black">
+							New list:
+						</label>
+						<input
+							type="text"
+							id="listName"
+							name="listName"
+							value={newListName}
+							onChange={handleChange}
+							className="border-2 border-sage rounded-lg min-h-14 bg-white grow text-xl xs:text-2xl text-off-black"
 						/>
-					);
-				})}
-			</ul>
+						<button className="bg-pale-green rounded-lg px-3 py-1 border-2 border-sage text-off-black">
+							Create
+						</button>
+					</form>
+					<ul className="flex flex-col my-8 items-center text-2xl">
+						{data.map((item, index) => {
+							return (
+								<SingleList
+									key={index}
+									name={item.name}
+									path={item.path}
+									setListPath={setListPath}
+								/>
+							);
+						})}
+					</ul>
+				</>
+			)}
 		</div>
 	);
 }
