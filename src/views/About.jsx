f023fcc996b07527369f8da49d7cf951d9a763eb
Shane Misra <shane.misra@gmail.com>
2024-04-09 22:25:52 -0600

    set up route for about page, no button yet

---
+++ src/views/About.jsx
@@ -0,0 +1,19 @@
+export function About() {
+	return (
+		<>
+			<section className="border border-dark-green py-8 px-12 text-off-black tiny:mt-14 items-center justify-center bg-sage/50 rounded-2xl">
+				<h1>About this application:</h1>
+				<p className="font-semibold">
+					Basket Buddy is a 'smart' shopping list application, that enables you
+					to customize your shopping experience the more you use it over time!
+					<br />
+					When a user indicates a purchase has been made, the application will
+					maintain that status for 24 full hours, enabling you to make sure that
+					every item on the list has been obtained - even through multiple
+					visits. You might also want to share a full list with others - Basket
+					Buddy enables that easily!
+				</p>
+			</section>
+		</>
+	);
+}
