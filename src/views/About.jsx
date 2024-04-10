3701aa009376a8783b6a0dfd46f76d4ace6666d7
Shane Misra <shane.misra@gmail.com>
2024-04-10 09:20:32 -0600

    mostly complete functionality, still needs adjustment for screen sizes

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,18 +1,77 @@
+import { IoLogoLinkedin } from 'react-icons/io5';
 export function About() {
 	return (
 		<>
-			<section className="border border-dark-green py-8 px-12 text-off-black tiny:mt-14 items-center justify-center bg-sage/50 rounded-2xl">
-				<h1>About this application:</h1>
-				<p className="font-semibold">
-					Basket Buddy is a 'smart' shopping list application, that enables you
-					to customize your shopping experience the more you use it over time!
-					<br />
-					When a user indicates a purchase has been made, the application will
-					maintain that status for 24 full hours, enabling you to make sure that
-					every item on the list has been obtained - even through multiple
-					visits. You might also want to share a full list with others - Basket
-					Buddy enables that easily!
+			<section className="flex flex-col border border-dark-green py-8 px-12 text-off-black tiny:mt-14 items-center justify-center bg-sage/50 rounded-2xl">
+				<h1 className="font-bold text-off-black my-2">
+					About this application:
+				</h1>
+				<p className="text-off-black my-2">
+					Basket Buddy is a smart shopping list application, that enables a user
+					to customize their shopping experience as it is used:
 				</p>
+				<li className="">
+					Share and complete lists with other users, with changes updated in
+					real time
+				</li>
+				<li>
+					Complete a list over multiple store visits, as you check items off a
+					list for 24 hours
+				</li>
+				<li>
+					Set future purchase dates for items, and BB will help determine the
+					best interval over time with each purchase
+				</li>
+			</section>
+			<section className="flex flex-col border border-dark-green py-8 px-12 text-off-black tiny:mt-14 items-center justify-center bg-sage/50 rounded-2xl">
+				<h2 className="font-bold text-off-black">The Creators:</h2>
+				<div>
+					<a
+						href="https://www.linkedin.com/in/andreapang/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<IoLogoLinkedin />
+					</a>
+					Andrea Pang
+				</div>
+				<div>
+					<a
+						href="https://www.linkedin.com/in/shanemisra/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<IoLogoLinkedin />
+					</a>
+					Shane Misra
+				</div>
+				<div>
+					<a
+						href="https://www.linkedin.com/in/emily-tiry/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<IoLogoLinkedin />
+					</a>
+					Emily Tiry
+				</div>
+				<div>
+					<a
+						href="https://www.linkedin.com/in/devina-gillis/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<IoLogoLinkedin />
+					</a>
+					Devina Gillis
+				</div>
+			</section>
+			<section className="flex flex-col border border-dark-green py-8 px-12 text-off-black tiny:mt-14 items-center justify-center bg-sage/50 rounded-2xl">
+				<h2 className="font-bold text-off-black">Special thanks to:</h2>
+				<span className="">Our mentors: Caitlyn, Luis, Raynaldo</span>
+				<a href="https://the-collab-lab.codes/">
+					<span>The Collab Lab</span>
+				</a>
 			</section>
 		</>
 	);
